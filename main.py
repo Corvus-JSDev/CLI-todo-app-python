634f76a6aa93563bb2d6a4ad29d8fa29d1c57d88
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-05 15:30:00 -0400

    add a edit command

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 todo_list = []
 
+help_commands ="""
+1) Add: Add a todo.
+2) Show: Show all your todos.
+3) Edit: Edit a single todo.
+4) Quit: Exit the program.
+"""
+
+
+print("type 'help' for a list of commands\n")
 while True:
-	user_command = input("Write a command (add | show | quit): ").lower()
+	user_command = input("Write a command: ").lower()
 
 	match user_command:
-		case "add":
-			# .title() will capitalize the first letter of each word.
+		case "help" | "h":
+			print(help_commands)
+
+		case "add" | "a":
+			# .title() will capitalize the first letter of 'every' word.
+			# .capitalize() will capitalize the first letter of the 'first' word
 			# .strip() will remove any trailing or leading spaces
-			todo = input("Enter a todo: ").title().strip()
+			todo = input("Enter a todo: ").capitalize().strip()
 			todo_list.append(todo)
 
-		case "show" :
+		case "show" | "display" | "s":
+			index = 0
 			for item in todo_list:
-				print(item, end=", ")
-			print(" ")
+				index += 1
+				print(f"{index}. {item}")
 
-		case "quit":
+		case "edit" | "e":
+			edit_choice = int(input(f'Choose a todo to edit (1 ... {len(todo_list)}): '))
+			if edit_choice > len(todo_list) or edit_choice <= 0:
+				print(f"That todo doesnt exist.")
+			else:
+				print(f'Editing: {todo_list[edit_choice - 1]}')
+				todo_list[edit_choice - 1] = input('New todo: ')
+
+		case "quit" | "q":
 			break
 
 		case _:
