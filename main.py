f501089f6201091d6c82d30cd052edbbda991752
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-05 16:00:11 -0400

    add a complete feature

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 todo_list = []
+completed_todos = 0
 
 help_commands ="""
 1) Add: Add a todo.
 2) Show: Show all your todos.
 3) Edit: Edit a single todo.
-4) Quit: Exit the program.
+4) Complete: Select what todo you have completed.
+5) Quit: Exit the program.
 """
 
 
@@ -25,6 +27,7 @@ while True:
 
 		case "show" | "display" | "s":
 			index = 0
+			print(f"You have completed {completed_todos} tasks.")
 			for item in todo_list:
 				index += 1
 				print(f"{index}. {item}")
@@ -35,7 +38,18 @@ while True:
 				print(f"That todo doesnt exist.")
 			else:
 				print(f'Editing: {todo_list[edit_choice - 1]}')
-				todo_list[edit_choice - 1] = input('New todo: ')
+				todo_list[edit_choice - 1] = input('New todo: ').capitalize()
+
+		case "complete" | "c":
+			completed_choice = int(input(f'Choose the todo you have finished (1 ... {len(todo_list)}): '))
+			if completed_choice > len(todo_list) or completed_choice <= 0:
+				print(f"That todo doesnt exist.")
+			else:
+				todo_list.remove(todo_list[completed_choice - 1])
+				print("Successfully completed a todo")
+				completed_todos += 1
+
+
 
 		case "quit" | "q":
 			break
