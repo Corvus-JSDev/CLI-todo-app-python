14f752cbeb172bb30d60441648cd6d39af9e7707
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-04 23:07:34 -0400

    refactor to use match-case

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 todo_list = []
 
 while True:
-	todo = input("Enter a todo (q to quit): ")
+	user_command = input("Write a command (add | show | quit): ").lower()
 
-	if todo == "q" or todo == "Q":
-		break
-	else:
-		todo_list.append(todo.title())  # .title() will capitalize the first letter of each word
+	match user_command:
+		case "add":
+			todo = input("Enter a todo: ").title()
+			todo_list.append(todo)
 
-print(todo_list)
+		case "show" :
+			for item in todo_list:
+				print(item, end=", ")
+			print(" ")
 
+		case "quit":
+			break
+
+		case _:
+			print(f'"{user_command}" is not a command.')
+
+print("\n\nYou have ended the program... goodbye")
 
 
 
