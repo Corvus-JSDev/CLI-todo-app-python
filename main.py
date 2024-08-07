4a2af10a0242541bd8c7ca05c71e1fd7d086c143
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-06 21:02:05 -0400

    refactored to use Context Managers and text style changes

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 completed_todos = 0
 
 help_commands ="""
-! 1) Add: Add a todo.
-* 2) Show: Show all your todos.
-? 3) Edit: Edit a single todo.
-TODO: 4) Complete: Select what todo you have completed.
+1) Add: Add a todo.
+2) Show: Show all your todos.
+3) Edit: Edit a single todo.
+4) Complete: Select what todo you have completed.
 5) Quit: Exit the program.
-
 """
 
 
 print("type 'help' for a list of commands\n")
 while True:
-	user_command = input("Write a command: ").lower()
+	user_command = input("\nWrite a command: ").lower()
 
 	match user_command:
 		case "help" | "h":
@@ -25,64 +24,55 @@ while True:
 
 			# Open the txt file and output its contents into the todo_list
 			# file.readlineS() will return a list with each line in the file being a new item
-			# file.readline() (with NO s) will only read a single line and return a string
-			file = open("user_todo.txt", "r")
-			todo_list = file.readlines()
-			todo_list.append(add_todo)
-			# It's important to close the file once you're done with it because other lines in the program may			interact with it, if it's still open
-			file.close()
+			with open("user_todo.txt", "r") as file:
+				todo_list = file.readlines()
+				todo_list.append(add_todo)
 
 			# Write (w) the contents of todo_list to the txt file
 			# The files will update when the program is ended
 			# Note: w will over-write the entire file
-			file = open('user_todo.txt', 'w')
-			file.writelines(todo_list)
-			file.close()
+			with open('user_todo.txt', 'w') as file:
+				file.writelines(todo_list)
 
 		case "show" | "display" | "s":
-			file = open("user_todo.txt", "r")
-			todo_list = file.readlines()
-			file.close()  # closing a file will not affect the contents of todo_list
+			# Context Managers are important to use because they not only take up less lines of code, but they will also use context to close any opened files if an errors are thrown
+			with open("user_todo.txt", "r") as file:
+				todo_list = file.readlines()
 
-			index = 0
-			print(f"You have completed {completed_todos} tasks.")
+			# print(f"\nYou have completed {completed_todos} tasks.")
+			print("\n------- TODOs -------")
 			if len(todo_list) == 0:
 				print("There are currently no todos")
 			else:
-				for item in todo_list:
-					index += 1
-					print(f"{index}. {item}", end="")
+				for index, item in enumerate(todo_list):
+					print(f"{index + 1}. {item}", end="")
 
 		case "edit" | "e":
-			file = open("user_todo.txt", "r")
-			todo_list = file.readlines()
-			file.close()
+			with open("user_todo.txt", "r") as file:
+				todo_list = file.readlines()
 
 			edit_choice = int(input(f'Choose a todo to edit (1 ... {len(todo_list)}): '))
 			if edit_choice > len(todo_list) or edit_choice <= 0:
 				print(f"That todo doesnt exist.")
 			else:
-				file = open("user_todo.txt", "w")
-				print(f'\nEditing: {todo_list[edit_choice - 1]}', end="")
-				todo_list[edit_choice - 1] = input('New todo: ').capitalize() + "\n"
-				file.writelines(todo_list)
-				file.close()
+				with open('user_todo.txt', 'w') as file:
+					print(f'\nEditing: {todo_list[edit_choice - 1]}', end="")
+					todo_list[edit_choice - 1] = input('New todo: ').capitalize() + "\n"
+					file.writelines(todo_list)
 
 		case "complete" | "c":
-			file = open("user_todo.txt", "r")
-			todo_list = file.readlines()
-			file.close()
+			with open('user_todo.txt', 'r') as file:
+				todo_list = file.readlines()
 
 			completed_choice = int(input(f'Choose the todo you have finished (1 ... {len(todo_list)}): '))
 			if completed_choice > len(todo_list) or completed_choice <= 0:
 				print(f"That todo doesnt exist.")
 			else:
-				file = open("user_todo.txt", "w")
-				todo_list.remove(todo_list[completed_choice - 1])
-				print("Successfully completed a todo")
-				completed_todos += 1
-				file.writelines(todo_list)
-				file.close()
+				with open('user_todo.txt', 'w') as file:
+					todo_list.remove(todo_list[completed_choice - 1])
+					file.writelines(todo_list)
+					# completed_todos += 1
+					print("Successfully completed a todo")
 
 		case "quit" | "q":
 			break
