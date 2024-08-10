d60f237ef663cb33b9016c0c3839b3ecdc59aa13
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-10 15:47:37 -0400

    add basic GUI layout

--- GUI.py
+++ GUI.py
@@ -1,3 +1,16 @@
-from funcs.functions import remove_first_word, get_todo, write_todo
-import FreeSimpleGUI
+# from funcs.functions import remove_first_word, get_todo, write_todo
+import FreeSimpleGUI as sg
 
+add_todo_label = sg.Text("Type in a ToDo:")
+inputbox = sg.InputText(tooltip="Enter a ToDo:")
+add_button = sg.Button("Add")
+
+window = sg.Window('My ToDo app',
+			 layout=[
+				 [add_todo_label],  # row 1
+				 [inputbox, add_button]  # row 2
+				 ],
+			 font=("inter", 14))
+
+window.read()
+window.close()
