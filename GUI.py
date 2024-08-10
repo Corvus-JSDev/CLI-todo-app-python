fc980716d947eacded733ebf76eb3d4e5b42e972
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-10 18:22:32 -0400

    added an edit button and list

--- GUI.py
+++ GUI.py
@@ -1,15 +1,21 @@
 from funcs.functions import *
 import FreeSimpleGUI as sg
 
+# Remove the last 2 characters (\n) from each item
+# todo_list = [item[:-1] for item in get_todo()]
+
 add_todo_label = sg.Text("Type in a ToDo:")
 inputbox = sg.InputText(tooltip="Enter a ToDo:", key="todo")
+list_box = sg.Listbox(values=[item[:-1] for item in get_todo()], key='todos', enable_events=True, size=(45, 10))
 add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
 quit_button = sg.Button("Quit")
 
 window = sg.Window('My ToDo app',
 			 layout=[
 				 [add_todo_label],  # row 1
 				 [inputbox, add_button],  # row 2
+				 [list_box, edit_button],
 				 [quit_button]
 				 ],
 			 font=("inter", 14))
@@ -29,13 +35,15 @@ print(value["todo"])  # hello world
 """
 
 while True:
+	# Start the GUI
 	event, value = window.read()
 
-	match event.lower():
-		case "quit":
+	# Read input values
+	match event:
+		case "Quit" | sg.WIN_CLOSED:
 			break
 
-		case "add":
+		case "Add":
 			add_todo = value["todo"].strip().capitalize()
 
 			todo_list = get_todo()
@@ -43,5 +51,10 @@ while True:
 
 			write_todo(todo_list)
 			print(f'\'{add_todo}\' has been added')
+
+		case "Edit":
+			edit_choice = value["todo"].strip()
+
+
 
 window.close()
