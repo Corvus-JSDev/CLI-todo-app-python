fedba05bc567312f6d4400a2923c8e988f83fbc7
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-10 16:07:33 -0400

    add quit btn

--- GUI.py
+++ GUI.py
@@ -4,11 +4,13 @@ import FreeSimpleGUI as sg
 add_todo_label = sg.Text("Type in a ToDo:")
 inputbox = sg.InputText(tooltip="Enter a ToDo:", key="todo")
 add_button = sg.Button("Add")
+quit_button = sg.Button("Quit")
 
 window = sg.Window('My ToDo app',
 			 layout=[
 				 [add_todo_label],  # row 1
-				 [inputbox, add_button]  # row 2
+				 [inputbox, add_button],  # row 2
+				 [quit_button]
 				 ],
 			 font=("inter", 14))
 
@@ -28,9 +30,12 @@ print(value["todo"])  # hello world
 
 while True:
 	event, value = window.read()
-	if value["todo"] == "quit":
-		break
-	else:
-		print(event, value["todo"])
+
+	match event.lower():
+		case "quit":
+			break
+
+		case "add":
+			print(value["todo"])
 
 window.close()
