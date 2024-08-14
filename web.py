a69599cabdfea32f87a267a165366ee68105ffbd
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-14 18:17:35 -0400

    Fix the same generated key bug

--- web.py
+++ web.py
@@ -1,14 +1,12 @@
 import streamlit as st
 from funcs.functions import *
 
-todo_list = get_todo()
-
 
 def add_todo():
 	new_todo = str(st.session_state["new_todo_input"]).strip().capitalize() + "\n"
 	todo_list.append(new_todo)
 	write_todo(todo_list)
-
+	st.session_state["new_todo_input"] = ""
 	print(f'\'{new_todo}\' has been added.')
 
 
@@ -19,8 +17,11 @@ st.write('meant to increase productivity')
 st.text_input(label='Enter a ToDo:', placeholder="Cook dinner", on_change=add_todo, key="new_todo_input")
 
 # Add the todos to the page
+todo_list = get_todo()
+todo_index = 0
 for todo in todo_list:
-	st.checkbox(todo)
+	st.checkbox(todo, key=todo_index)
+	todo_index += 1
 
 
-st.session_state
+# st.session_state
