f5b02f6357829321e90878423a9d47a37ebf5148
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-14 17:28:24 -0400

    Testing out streamlit

---
+++ web.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from funcs.functions import *
+
+st.title('My todo app')
+st.subheader('This is a simple todo app')
+st.write('meant to increase productivity')
+
+st.checkbox('This is a test checkbox')
